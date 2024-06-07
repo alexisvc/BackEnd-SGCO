1976e5b46debecb094b44f6a21f71e0f8b7db9eb
Alexis Vizuete <alexis.vizuete@epn.edu.ec>
2024-06-07 11:45:20 -0500

    Update patients

--- controllers/patients.js
+++ controllers/patients.js
@@ -1,90 +1,144 @@
-const express = require('express')
-const patientsRouter = express.Router()
-const Patient = require('../models/Patient')
+const express = require('express');
+const patientsRouter = express.Router();
+const Patient = require('../models/Patient');
 
 // Ruta para obtener todos los pacientes
 patientsRouter.get('/', async (req, res) => {
   try {
-    const patients = await Patient.find().populate('medicalRecords')
-    res.json(patients)
+    const patients = await Patient.find().populate('medicalRecords');
+    res.json(patients);
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' })
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-})
+});
 
 // Ruta para obtener un paciente por su ID
 patientsRouter.get('/:id', async (req, res) => {
   try {
-    const patientId = req.params.id
-    const patient = await Patient.findById(patientId)
+    const patientId = req.params.id;
+    const patient = await Patient.findById(patientId);
     if (!patient) {
-      return res.status(404).json({ error: 'Patient not found' })
+      return res.status(404).json({ error: 'Patient not found' });
     }
-    res.json(patient)
+    res.json(patient);
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' })
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-})
+});
+
+// Ruta para obtener un paciente por número de cédula
+patientsRouter.get('/cedula/:numeroCedula', async (req, res) => {
+  try {
+    const numeroCedula = req.params.numeroCedula;
+    const patient = await Patient.findOne({ numeroCedula });
+    if (!patient) {
+      return res.status(404).json({ error: 'Patient not found' });
+    }
+    res.json(patient);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 // Ruta para registrar un nuevo paciente
 patientsRouter.post('/', async (req, res) => {
   try {
-    const { name, age, gender } = req.body
+    const {
+      nombrePaciente,
+      edadPaciente,
+      fechaNacimiento,
+      correoPaciente,
+      direccionPaciente,
+      generoPaciente,
+      numeroCedula,
+      ocupacion,
+      telefono,
+      telContactoEmergencia,
+      afinidadContactoEmergencia,
+    } = req.body;
 
-    if (!name || !age || !gender) {
-      return res.status(400).json({ error: 'All fields are required' })
+    if (!nombrePaciente || !edadPaciente || !fechaNacimiento || !correoPaciente || !direccionPaciente || !generoPaciente || !numeroCedula || !ocupacion || !telefono || !telContactoEmergencia || !afinidadContactoEmergencia) {
+      return res.status(400).json({ error: 'All fields are required' });
     }
 
     const patient = new Patient({
-      name,
-      age,
-      gender
-    })
+      nombrePaciente,
+      edadPaciente,
+      fechaNacimiento,
+      correoPaciente,
+      direccionPaciente,
+      generoPaciente,
+      numeroCedula,
+      ocupacion,
+      telefono,
+      telContactoEmergencia,
+      afinidadContactoEmergencia
+    });
 
-    const savedPatient = await patient.save()
-    res.json(savedPatient)
+    const savedPatient = await patient.save();
+    res.json(savedPatient);
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Internal Server Error' })
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-})
+});
 
 // Ruta para actualizar un paciente por su ID
 patientsRouter.put('/:id', async (req, res) => {
   try {
-    const patientId = req.params.id
-    const { name, age, gender } = req.body
+    const patientId = req.params.id;
+    const {
+      nombrePaciente,
+      edadPaciente,
+      fechaNacimiento,
+      correoPaciente,
+      direccionPaciente,
+      generoPaciente,
+      numeroCedula,
+      ocupacion,
+      telefono,
+      telContactoEmergencia,
+      afinidadContactoEmergencia
+    } = req.body;
 
-    const existingPatient = await Patient.findById(patientId)
+    const existingPatient = await Patient.findById(patientId);
     if (!existingPatient) {
-      return res.status(404).json({ error: 'Patient not found' })
+      return res.status(404).json({ error: 'Patient not found' });
     }
 
-    if (name) existingPatient.name = name
-    if (age) existingPatient.age = age
-    if (gender) existingPatient.gender = gender
+    if (nombrePaciente) existingPatient.nombrePaciente = nombrePaciente;
+    if (edadPaciente) existingPatient.edadPaciente = edadPaciente;
+    if (fechaNacimiento) existingPatient.fechaNacimiento = fechaNacimiento;
+    if (correoPaciente) existingPatient.correoPaciente = correoPaciente;
+    if (direccionPaciente) existingPatient.direccionPaciente = direccionPaciente;
+    if (generoPaciente) existingPatient.generoPaciente = generoPaciente;
+    if (numeroCedula) existingPatient.numeroCedula = numeroCedula;
+    if (ocupacion) existingPatient.ocupacion = ocupacion;
+    if (telefono) existingPatient.telefono = telefono;
+    if (telContactoEmergencia) existingPatient.telContactoEmergencia = telContactoEmergencia;
+    if (afinidadContactoEmergencia) existingPatient.afinidadContactoEmergencia = afinidadContactoEmergencia;
 
-    const updatedPatient = await existingPatient.save()
-    res.json(updatedPatient)
+    const updatedPatient = await existingPatient.save();
+    res.json(updatedPatient);
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' })
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-})
+});
 
 // Ruta para eliminar un paciente por su ID
 patientsRouter.delete('/:id', async (req, res, next) => {
   try {
-    const id = req.params.id
-    const deletedPatient = await Patient.findByIdAndDelete(id)
+    const id = req.params.id;
+    const deletedPatient = await Patient.findByIdAndDelete(id);
 
     if (!deletedPatient) {
-      return res.status(404).json({ error: 'Patient not found' })
+      return res.status(404).json({ error: 'Patient not found' });
     }
 
-    res.status(204).end()
+    res.status(204).end();
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-module.exports = patientsRouter
+module.exports = patientsRouter;
--- models/Patient.js
+++ models/Patient.js
@@ -1,17 +1,49 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const patientSchema = new mongoose.Schema({
-  name: {
+  nombrePaciente: {
     type: String,
     required: true
   },
-  age: {
+  edadPaciente: {
     type: Number,
     required: true
   },
-  gender: {
+  fechaNacimiento: {
+    type: Date,
+    required: true
+  },
+  correoPaciente: {
+    type: String,
+    required: true
+  },
+  direccionPaciente: {
+    type: String,
+    required: true
+  },
+  generoPaciente: {
     type: String,
     enum: ['male', 'female', 'other'],
+    required: true
+  },
+  numeroCedula: {
+    type: String,
+    required: true
+  },
+  ocupacion: {
+    type: String,
+    required: true
+  },
+  telefono: {
+    type: String,
+    required: true
+  },
+  telContactoEmergencia: {
+    type: String,
+    required: true
+  },
+  afinidadContactoEmergencia: {
+    type: String,
     required: true
   },
   medicalRecords: [
@@ -20,16 +52,16 @@ const patientSchema = new mongoose.Schema({
       ref: 'MedicalRecord'
     }
   ]
-})
+});
 
 patientSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-})
+});
 
-const Patient = mongoose.model('Patient', patientSchema)
+const Patient = mongoose.model('Patient', patientSchema);
 
-module.exports = Patient
+module.exports = Patient;
